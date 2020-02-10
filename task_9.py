006384ab68d17a798025e991137c3421c95a3f64
Максим Д. Шинкевич <shinkevich_md@edu.kvantorium-pk.ru>
2020-02-10 17:32:00 +1000

    done

--- task_9.py
+++ task_9.py
@@ -5,8 +5,15 @@ from pyrob.api import *
 
 @task
 def task_8_2():
+    while wall_is_above() or wall_is_beneath():
+        move_right()
+        if not wall_is_above() and wall_is_beneath():
+            fill_cell()
+
     pass
 
 
 if __name__ == '__main__':
     run_tasks()
+
+
